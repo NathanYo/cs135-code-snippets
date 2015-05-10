1c7499c0d19de74a4086dcf7b09b1c116586705c
Nathan Yocum <nayocum@nevada.unr.edu>
2015-05-10 00:18:49 -0700

    Update q5ans1.cpp
    Formatting

--- quizzes/q5/q5ans1.cpp
+++ quizzes/q5/q5ans1.cpp
@@ -2,15 +2,15 @@
 
 int foo(int &a, int &x){
       x++;
-        a--;
-          return 2 * x + a;
+      a--;
+      return 2 * x + a;
 }
 
 int main(){
       int x = 1;
-        int y = 2;
-          int z = foo(x, y);
-            printf ("%d, %d, %d\n", x, y, z);
-              return 0;
+      int y = 2;
+      int z = foo(x, y);
+      printf ("%d, %d, %d\n", x, y, z);
+      return 0;
 }
 
